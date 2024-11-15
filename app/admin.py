6e4f69ee1044541bd93e08491c76c3aa126d46a0
Sreenath k k <sreenathkk00@gmail.com>
2024-11-15 22:07:26 +0530

    admin page configured

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Product, CartItem
+from django.utils.html import format_html
 
-# Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'description','category', 'product_image_display')  
+    search_fields = ('name', 'description')  
+    # Custom method to display product image as clickable
+    def product_image_display(self, obj):
+        if obj.image:  
+            return format_html(
+                '<a href="{}" target="_blank"><img src="{}" width="50" height="50" style="object-fit: cover;" /></a>',
+                obj.image.url,  # Link to the full-size image
+                obj.image.url   # Thumbnail image
+            )
+        return '-'
+    
+    product_image_display.short_description = 'Product Image'
+admin.site.register(Product, ProductAdmin)
+
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ('product', 'user', 'quantity', 'get_total_price')
+    search_fields = ('product__name', 'user__username')
+    
+
+admin.site.register(CartItem, CartItemAdmin)
